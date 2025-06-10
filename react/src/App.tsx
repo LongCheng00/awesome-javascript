7be041d70514607d402c4c5950acfb4e011718bd
luo.reid <luo.reid@gmail.com>
2025-06-10 22:57:41 +0800

    feat: react quick recall

--- react/src/App.tsx
+++ react/src/App.tsx
@@ -3,12 +3,77 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+function Products() {
+  const products = [
+    { id: 1, name: 'Laptop', price: 999.99 },
+    { id: 2, name: 'Smartphone', price: 499.99, isSmart: true },
+    { id: 3, name: 'Tablet', price: 299.99 },]
+  const listItems = products.map((product) =>
+    <li key={product.id}
+      style={{ color: product.isSmart ? 'magenta' : 'darkgreen' }}>
+      {product.name}</li>
+  )
+  return (
+    <ul>{listItems}</ul>
+  )
+}
 
+function UserProfile() {
+  const user = {
+    name: 'Hedy Lamarr',
+    imageUrl: 'https://i.imgur.com/yXOvdOSs.jpg',
+    imageSize: 90,
+    count: 0,
+  };
   return (
     <>
+      <h1>{user.name} </h1>
+      <img
+        className="avatar"
+        src={user.imageUrl}
+        alt={'Photo of ' + user.name}
+        style={{
+          width: user.imageSize,
+          height: user.imageSize,
+          borderRadius: '50%',
+          border: '2px dashed green'
+        }}
+      />
+    </>
+  )
+}
+
+function MyButton({ count, onClick }) {
+  const [sum, setSum] = useState(0)
+
+  const handleClick = () => {
+    setSum(sum + 1)
+  };
+  return (
+    <>
+      <button className="my-button" onClick={onClick}>
+        Click Parent's Count: {count}</button>
+
+      <button className="my-button" onClick={handleClick}>
+        Click Me
+      </button>
+      {sum}
+    </>
+  )
+}
+
+function App() {
+  const [count, setCount] = useState(0)
+  function handleClick() {
+    setCount(count + 1)
+  }
+  return (
+    <>
+      <UserProfile />
       <div>
+        <MyButton count={count} onClick={handleClick} />
+        <Products />
+        <MyButton count={count} onClick={handleClick} />
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
