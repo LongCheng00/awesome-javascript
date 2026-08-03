c800e88ccfd605672a074015806e814e35730e8b
luo.reid <luo.reid@gmail.com>
2026-08-03 22:59:01 +0800

    feat: add to cart

--- apps/SportsStore/src/app/model/model.module.ts
+++ apps/SportsStore/src/app/model/model.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
 import { ProductRepository } from './product.repository';
 import { StaticDataSource } from './static.datasource';
+import { Cart } from './cart.model';
 
 @NgModule({
-  providers: [StaticDataSource, ProductRepository],
+  providers: [StaticDataSource, ProductRepository, Cart],
 })
 export class ModelModule {}
--- apps/SportsStore/src/app/store/store.component.ts
+++ apps/SportsStore/src/app/store/store.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { Product } from '../model/product.model';
 import { ProductRepository } from '../model/product.repository';
+import { Cart } from '../model/cart.model';
 
 @Component({
   standalone: false,
@@ -12,17 +13,20 @@ export class StoreComponent {
   public productsPerPage = 4;
   public selectedPage = 1;
 
-  constructor(private readonly productRepository: ProductRepository) {}
+  constructor(
+    private readonly repository: ProductRepository,
+    private cart: Cart,
+  ) {}
 
   get products(): Product[] {
     let pageIndex = (this.selectedPage - 1) * this.productsPerPage;
-    return this.productRepository
+    return this.repository
       .getProducts(this.selectedCategory)
       .slice(pageIndex, pageIndex + this.productsPerPage);
   }
 
   get categories(): string[] {
-    return this.productRepository.getCategories();
+    return this.repository.getCategories();
   }
 
   changeCategory(newCategory?: string) {
@@ -40,16 +44,18 @@ export class StoreComponent {
   }
   get pageNumbers(): number[] {
     return Array(
-      Math.ceil(
-        this.productRepository.getProducts(this.selectedCategory).length / this.productsPerPage,
-      ),
+      Math.ceil(this.repository.getProducts(this.selectedCategory).length / this.productsPerPage),
     )
       .fill(0)
       .map((x, i) => i + 1);
   }
   get pageCount(): number {
     return Math.ceil(
-      this.productRepository.getProducts(this.selectedCategory).length / this.productsPerPage,
+      this.repository.getProducts(this.selectedCategory).length / this.productsPerPage,
     );
+  }
+
+  addProductToCart(product: Product) {
+    this.cart.addLine(product);
   }
 }
